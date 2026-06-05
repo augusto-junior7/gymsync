c6833a93b9adf4d081517ce18108c14a4f39214d
Augusto Roberto Tavares Júnior <augustojhunior@gmail.com>
2026-06-05 15:13:10 -0300

    configurações iniciais do frontend

---
+++ frontend/src/pages/Perfil.jsx
@@ -0,0 +1,7 @@
+export default function Perfil() {
+  return (
+    <div>
+      <h1>Página de Perfil</h1>
+    </div>
+  )
+}
---
+++ frontend/src/pages/Registrar.jsx
@@ -0,0 +1,7 @@
+export default function Registrar() {
+  return (
+    <div>
+      <h1>Página de Registro</h1>
+    </div>
+  )
+}
