a2400c3855380da766d3bcfdddd7d3b9cfd752fb
Gahenrique64 <gabrielhenriquegonzaga645@gmail.com>
2026-06-10 22:29:53 -0300

    Criação da rota de filtro de exercício por grupo muscular

--- backend/src/controllers/exerciciosController.js
+++ backend/src/controllers/exerciciosController.js
@@ -1,1 +1,21 @@
 import Exercicio from "../models/exercicio.js"
+
+export const exercicios = async (req, res) => {
+    try{
+        let filtro = {}
+
+        // verificação para ver se o frontend mandou o parametro "grupo" na URL
+        if (req.query.grupo) {
+            
+        // extraindo o pedido do filtro pela URL (GET com form HTML)
+        filtro.musculosPrincipais = req.query.grupo;
+        }
+
+        // Busca no banco de dados os exercícios com esse filtro
+        const exerciciosEncontrados = await Exercicio.find(filtro)
+        return res.status(200).json({quantidade: exerciciosEncontrados.length, dados: exerciciosEncontrados})
+
+    }catch(error) {
+        res.status(500).json({mensagem: 'Erro interno ao buscar exercícios'})
+    }
+}
--- backend/src/routes/exercicio.routes.js
+++ backend/src/routes/exercicio.routes.js
@@ -1,8 +1,9 @@
 import { Router } from 'express'
 import Exercicio from '../models/exercicio.js'
+import { exercicios } from '../controllers/exerciciosController.js'
 
 const router = Router()
 
-router.get("/exercicios", )
+router.get('/exercicios', exercicios)
 
 export default router
